c0024f8c4a37dfdfa77f07a0dc719a054efcc60f
maddyboi <81882255+maddyboi@users.noreply.github.com>
2021-09-30 11:00:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-self.addEventListener('install',function(event))
-{
-                      event.waitUntill(caches.open('sw-cache').then(function(cache){
-                        return cache.add('index.html');
-})
-);
+
+
+self.addEventListener('install',function(event){
+ event.waitUntill(caches.open(sw-cahce).then(function(cache){
+
+  return cache.add('index.html');
+ }))
 });
 
-self.addEventListener('fetch',function(event))
-{
-                      event.respondWith(caches.match(event.request).then(function(response){
-                        return response || fetch(event.request);
-})
-);
-});
-
+self.addEventListener('fetch',function(event){
+  event.respondWith(caches.match(event.request).then(function(response){
+ 
+   return response || fetch(event.request);
+  }))
+ });
